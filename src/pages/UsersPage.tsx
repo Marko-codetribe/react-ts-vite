5a867c70a064131aaa69facb14d9a1d407182c5a
MarkoRakonjac <marko.rakonjac@codetri.be>
2023-04-06 12:57:28 +0200

    feat: update husky pre-commit

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -3,5 +3,5 @@ import { useTranslation } from "react-i18next"
 export default function UsersPage() {
   const { t } = useTranslation()
 
-  return <h1>{t("user_page.title")} test</h1>
+  return <h1>{t("user_page.title")}</h1>
 }
